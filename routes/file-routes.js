8ebc1ef155a8fb84fbe28c5a4d7fe92dd7e1f715
Bay Foley-Cox <bayfoleycox@gmail.com>
2018-08-15 15:32:25 -0400

    Fixed bug where using a bad ID to access a file would cause a crash.

--- routes/file-routes.js
+++ routes/file-routes.js
@@ -10,18 +10,23 @@ router.get('/badge-defs/:badgeDefId', (req, res) => {
   var fullURL = req.protocol + '://' + req.get('host') + req.originalUrl;
   Badge.findById(badgeDefId, (err, badgeClass) => {
     //$at$ is replaced with @ when rendered
-    var badgeClassJSON = {
-      '@context': 'https://w3id.org/openbadges/v2',
-      id: fullURL,
-      type: 'BadgeClass',
-      name: badgeClass.get('name'),
-      description: badgeClass.get('description'),
-      image: badgeClass.get('image'),
-      criteria: badgeClass.get('criteria'),
-      issuer: badgeClass.get('issuer'),
-      tags: badgeClass.get('tags')
+    if(badgeClass) {
+      var badgeClassJSON = {
+        '@context': 'https://w3id.org/openbadges/v2',
+        id: fullURL,
+        type: 'BadgeClass',
+        name: badgeClass.get('name'),
+        description: badgeClass.get('description'),
+        image: badgeClass.get('image'),
+        criteria: badgeClass.get('criteria'),
+        issuer: badgeClass.get('issuer'),
+        tags: badgeClass.get('tags')
+      }
+      res.send(badgeClassJSON)
     }
-    res.send(badgeClassJSON)
+    else {
+      res.send('No badge class found for that ID');
+    }
   });
 });
 
@@ -30,21 +35,26 @@ router.get('/assertions/:assertionId', (req, res) => {
   var fullURL = req.protocol + '://' + req.get('host') + req.originalUrl;
   var dateString = new Date().toISOString();
   Assertion.findById(assertionId, (err, assertion) => {
-    var assertionJSON = {
-      '@context': 'https://w3id.org/openbadges/v2',
-      id: fullURL,
-      type: 'Assertion',
-      recipient: {
-        type: 'name',
-        identity: assertion.get('recipient'),
-      },
-      issuedOn: dateString,
-      verification: {
-        type: 'hosted'
-      },
-      badge: assertion.get('badge')
+    if(assertion) {
+      var assertionJSON = {
+        '@context': 'https://w3id.org/openbadges/v2',
+        id: fullURL,
+        type: 'Assertion',
+        recipient: {
+          type: 'name',
+          identity: assertion.get('recipient'),
+        },
+        issuedOn: dateString,
+        verification: {
+          type: 'hosted'
+        },
+        badge: assertion.get('badge')
+      }
+      res.send(assertionJSON);
     }
-    res.send(assertionJSON);
+    else {
+      res.send('No assertion found for that ID');
+    }
   });
 });
 
@@ -52,15 +62,20 @@ router.get('/profiles/:profileId', (req, res) => {
   var profileId = req.params.profileId;
   var fullURL = req.protocol + '://' + req.get('host') + req.originalUrl;
   Profile.findById(profileId, (err, profile) => {
-    var profileJSON = {
-      '@context': 'https://w3id.org/openbadges/v2',
-      id: fullURL,
-      type: 'issuer',
-      name: profile.get('name'),
-      url: profile.get('url'),
-      description: profile.get('description')
+    if(profile) {
+      var profileJSON = {
+        '@context': 'https://w3id.org/openbadges/v2',
+        id: fullURL,
+        type: 'issuer',
+        name: profile.get('name'),
+        url: profile.get('url'),
+        description: profile.get('description')
+      }
+      res.send(profileJSON);
     }
-    res.send(profileJSON);
+    else {
+      res.send('No profile found for that ID');
+    }
   });
 });
 
